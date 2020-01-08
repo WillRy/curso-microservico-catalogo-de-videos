abc050f3a0797046ac7039eb04d729ad3a7d04d5
WillRy <wiribasczky@gmail.com>
2020-01-08 20:23:55 -0300

    Criado primeiro teste unitário da model: Category
    Testes aplicados:
    -Se há os atributos fillable
    -Se há as traits softDeletes e Uuid
    -Se há o casts e se estão iguais
    -Se o auto incremento está desabilitado
    -Se há os atributos de data

---
+++ tests/Unit/CategoryTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Category;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use PHPUnit\Framework\TestCase;
+
+class CategoryTest extends TestCase
+{
+
+    public function testFillableAttribute()
+    {
+        $category = new Category();
+        $fillable = ['name', 'description', 'is_active'];
+        $this->assertEquals($fillable, $category->getFillable());
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [
+            SoftDeletes::class,
+            Uuid::class
+        ];
+        $categoryTraits = array_keys(class_uses(Category::class));
+        foreach ($traits as $trait) {
+            $this->assertContains($trait, $categoryTraits);
+        }
+        $this->assertCount(count($traits), $categoryTraits);
+    }
+
+    public function testCastsAttribute()
+    {
+        $category = new Category();
+        $casts = ['id' => 'string'];
+        $this->assertEquals($casts, $category->getCasts());
+    }
+
+    public function testIncrementingAttribute()
+    {
+        $category = new Category();
+        $this->assertFalse($category->incrementing);
+    }
+
+    public function testDatesAttribute()
+    {
+        $category = new Category();
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        foreach ($dates as $date) {
+            $this->assertContains($date, $category->getDates());
+        }
+        $this->assertCount(count($dates), $category->getDates());
+    }
+
+}
